10d36cd44889f391416428da0e91e1a57f270c2c
secureuplink <git@secureup.link>
2020-08-02 08:10:25 +0100

    Initial commit

---
+++ faces.py
@@ -0,0 +1,16 @@
+import requests
+import shutil
+
+from time import sleep
+
+#settings
+url = "https://thispersondoesnotexist.com/image"
+delay = 5 #seconds
+
+def download_face():
+    response = requests.get(url, stream=True)
+    with open('img.jpg', 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    return response
+
+download_face()
